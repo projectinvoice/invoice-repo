1b37cd6555380ea00279c72d12556dd8300ab0ba
projectinvoice <project.invoice.mail@gmail.com>
2026-04-06 22:02:09 +0000

    je vien d'ajouter les differentes vues pour la modification des differentes classes du models.py

--- InvoiceProject/InvoiceProject/urls.py
+++ InvoiceProject/InvoiceProject/urls.py
@@ -16,7 +16,35 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from InvoiceApp import views as invoice_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', invoice_views.register_company, name='register'),
+    path('login/', invoice_views.login_view, name='login'),
+    path('change-password/', invoice_views.change_password, name='change_password'),
+    path('delete-account/', invoice_views.delete_account, name='delete_account'),
+
+    path('add-agent-role/', invoice_views.add_agent_role, name='add_agent_role'),
+    path('delete-agent-role/', invoice_views.delete_agent_role, name='delete_agent_role'),
+    path('add-agent/', invoice_views.add_agent, name='add_agent'),
+    path('delete-agent/', invoice_views.delete_agent, name='delete_agent'),
+    path('add-engine/', invoice_views.add_engine, name='add_engine'),
+    path('delete-engine/', invoice_views.delete_engine, name='delete_engine'),
+    path('add-product/', invoice_views.add_product, name='add_product'),
+    path('delete-product/', invoice_views.delete_product, name='delete_product'),
+    path('add-client/', invoice_views.add_client, name='add_client'),
+    path('delete-client/', invoice_views.delete_client, name='delete_client'),
+    path('add-payment-type/', invoice_views.add_payment_type, name='add_payment_type'),
+    path('delete-payment-type/', invoice_views.delete_payment_type, name='delete_payment_type'),
+    path('add-payment-method/', invoice_views.add_payment_method, name='add_payment_method'),
+    path('delete-payment-method/', invoice_views.delete_payment_method, name='delete_payment_method'),
+    path('add-supplier/', invoice_views.add_supplier, name='add_supplier'),
+    path('delete-supplier/', invoice_views.delete_supplier, name='delete_supplier'),
+    path('add-supply/', invoice_views.add_supply, name='add_supply'),
+    path('delete-supply/', invoice_views.delete_supply, name='delete_supply'),
+    path('add-sale/', invoice_views.add_sale, name='add_sale'),
+    path('delete-sale/', invoice_views.delete_sale, name='delete_sale'),
+    path('add-invoice/', invoice_views.add_invoice, name='add_invoice'),
+    path('delete-invoice/', invoice_views.delete_invoice, name='delete_invoice'),
 ]
