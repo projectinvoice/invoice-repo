d5757e8885e2786cd4d1b03481f903ba859ea468
projectinvoice <project.invoice.mail@gmail.com>
2026-08-11 08:07:37 +0000

    modifications majeurs

--- InvoiceProject/InvoiceProject/urls.py
+++ InvoiceProject/InvoiceProject/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path('login/', invoice_views.login_view, name='login'),
     path('logout/', invoice_views.logout_view, name='logout'),
     path('dashboard/', invoice_views.dashboard, name='dashboard'),
+    path('api/dashboard/ca/', invoice_views.api_dashboard_ca, name='api_dashboard_ca'),
+    path('company-settings/', invoice_views.company_settings, name='company_settings'),
     path('manage-agent-roles/', invoice_views.manage_agent_roles, name='manage_agent_roles'),
     
     # List views for all entities
@@ -81,7 +83,6 @@ urlpatterns = [
     path('stock-loads/', invoice_views.list_stock_loads, name='list_stock_loads'),
     path('add-stock-load/', invoice_views.add_stock_load, name='add_stock_load'),
     path('add-stock-return/', invoice_views.add_stock_return, name='add_stock_return'),
-    path('company-settings/', invoice_views.company_settings, name='company_settings'),
 
 ]
 
